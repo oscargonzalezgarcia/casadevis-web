cc94735df2dd595cdb0721d62dcdd233d94d4f55
oscargonzalezgarcia <oscar.goga10@gmail.com>
2024-01-14 16:09:53 +0100

    #CDV-3 Added Flowbite React

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import { useState } from "react";
 import logo from "./logo.svg";
+import { FloatingLabel } from "flowbite-react";
 
 export default function App() {
   const [step, setStep] = useState(1);
   const steps = Array.from({ length: 4 }, (_, index) => index + 1);
 
   function nextStep() {
-    setStep((prevStep) => prevStep == 4 ? 1 : prevStep + 1);
+    setStep((prevStep) => (prevStep == 4 ? 1 : prevStep + 1));
   }
 
   return (
     <div className="font-manrope bg-green-100">
-      <div className="bg-gray-200 p-6 rounded-lg w-full text-center">
-        <div className="grid grid-cols-4 gap-x-4 w-[15%] mx-auto">
+      <div className="bg-gray-200 p-6 rounded-lg w-full">
+        <div className="grid grid-cols-4 gap-x-4 w-[15%] mx-auto text-center">
           {steps.map((currentStep) => (
             <div
               key={currentStep}
@@ -26,38 +27,20 @@ export default function App() {
             </div>
           ))}
         </div>
-        <h1 className="text-4xl font-bold mb-4">Casa de Vis</h1>
-        <h2 className="text-2xl mb-4">Tasa tu inmueble</h2>
-        <div className="mb-4">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="name"
-          >
-            Nombre
-          </label>
-          <input
-            className="border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
-            id="name"
-            type="text"
-            placeholder="Enter your name"
-          />
+        <div className="text-center">
+          <h1 className="text-4xl font-bold mb-4">Casa de Vis</h1>
+          <h2 className="text-2xl mb-4">Tasa tu inmueble</h2>
         </div>
-        <div className="mb-4">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="email"
-          >
-            Correo
-          </label>
-          <input
-            className="border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
-            id="email"
-            type="email"
-            placeholder="Enter your email"
-          />
+        <div className="w-[50%]">
+          <div className="mb-4">
+            <FloatingLabel variant="standard" label="Nombre"></FloatingLabel>
+          </div>
+          <div className="mb-4">
+            <FloatingLabel variant="standard" label="Correo"></FloatingLabel>
+          </div>
         </div>
         <button
-          className="bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          className="bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline hover:bg-blue-400 active:bg-white active:text-black"
           type="button"
           onClick={nextStep}
         >
