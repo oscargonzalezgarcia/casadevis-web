4c29d1eee3b69bcf89350d43bcdf21665c520c21
oscargonzalezgarcia <oscar.goga10@gmail.com>
2024-01-21 20:33:12 +0100

    #CDV-4 Refactored TopNavbar

--- src/components/TopNavbar.tsx
+++ src/components/TopNavbar.tsx
@@ -1,17 +1,16 @@
-
-import { Navbar } from "flowbite-react";
 import React from "react";
 
 export default function TopNavbar() {
   return (
-    <Navbar fluid className="bg-gray-900">
-      <Navbar.Brand href="https://flowbite-react.com" className="gap-x-4">
-        <img src={process.env.PUBLIC_URL + "/images/devo-logo.png"} width={24} height={24} />
-        <span className="font-manrope self-center whitespace-nowrap text-2xl font-bold text-white">
-          Casa de Vis
-        </span>
-      </Navbar.Brand>
-      <Navbar.Toggle />
-    </Navbar>
+    <div className="flex items-center justify-center py-4 gap-x-4 bg-gray-900">
+      <img
+        src={process.env.PUBLIC_URL + "/images/devo-logo.png"}
+        width={30}
+        height={30}
+      />
+      <h1 className="font-manrope self-center whitespace-nowrap text-3xl font-bold text-white">
+        Casa de Vis
+      </h1>
+    </div>
   );
 }
